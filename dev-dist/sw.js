84035019367dac43cdb1384c5f669347b5518309
Mcjavier123RV <2122100612@soy.utj.edu.mx>
2025-11-30 22:04:21 -0600

    3re commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9u10cj8ne38"
+    "revision": "0.ivj8p0p7beg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
